03fc34e52b71c5acdbae196dc70ebf9613a3b628
bhenbe <contact@bhen.be>
2018-05-29 19:32:14 +0200

    Add save to file function

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,8 @@ var app = new Vue({
       userphone: '',
       usermail: '',
       usermaillink: '',
-      code: ''
+      code: '',
+      textFile: null
     },
     watch: {
         usermail: function (newValue, oldValue) {
@@ -17,16 +18,27 @@ var app = new Vue({
         setCode: function(){
             this.code = document.getElementById("code").innerHTML;
             this.code = this.code.trim();
+            var link = document.getElementById('download_link');
+            link.href = this.downloadCode(this.code);
         },
         updateCode: function(evt){
             this.code = document.getElementById("code").innerHTML;
             this.code = this.code.trim();
+            var link = document.getElementById('download_link');
+            link.href = this.downloadCode(this.code);
+            console.log(link.href);
         },
         saveCode: function(evt){
             document.getElementById("input-code").select();
             document.execCommand("copy");
             alert('Vous pouvez coller votre code !');
             return false;
+        },
+        downloadCode: function(the_code) {
+            var code_file = new Blob([the_code], {type: 'text/plain'});
+            if (this.textFile !== null) { window.URL.revokeObjectURL(this.textFile); }
+            this.textFile = window.URL.createObjectURL(code_file);
+            return this.textFile;
         }
     }
   })
